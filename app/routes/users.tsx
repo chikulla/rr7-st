802f4a5d78b02a19a7bfb07357a50690bea1b7ba
chikulla <3042031+chikulla@users.noreply.github.com>
2025-09-12 23:27:05 +0900

    submit with RHF

--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,5 +1,10 @@
-import { Form } from "react-router";
 import type { Route } from "./+types/users";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+import { useForm } from "react-hook-form";
+import { useSubmit } from "react-router";
+import { useEffect } from "react";
 
 let mem = [
     { id: 1, name: "Taro Tanaka", email: "taro@example.com" },
@@ -23,20 +28,54 @@ export async function action({ request }: Route.ActionArgs): Promise<ActionData>
 }
 export default function Users({ loaderData, actionData }: Route.ComponentProps) {
     const { users } = loaderData;
+    const form = useForm<{ name: string, email: string }>({ defaultValues: { name: "", email: "" } });
+    const submit = useSubmit();
+    function onSubmit(data: { name: string, email: string }) {
+        console.log("onSubmit: ", data);
+        const fd = new FormData();
+        fd.set("name", data.name);
+        fd.set("email", data.email);
+        submit(fd, { method: "post" });
+    }
+    useEffect(() => {
+        if (actionData?.created) {
+            form.reset()
+        }
+    }, [actionData, form])
     return (
         <div>
             <h1>Users</h1>
-            <Form method="post">
-                <input name="name" placeholder="name" type="text" />
-                <input name="email" placeholder="email" type="email" />
-                <button type="submit">Add</button>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mb-8">
+                    <FormField control={form.control} name="name" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="name" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )} />
+                    <FormField control={form.control} name="email" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="email" type="email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )} />
+                    <Button type="submit" disabled={form.formState.isSubmitted}>Add</Button>
+                </form>
             </Form>
 
-            {actionData?.error ? (
-                <p style={{ color: "red" }}>{actionData.error}</p>
-            ) : actionData?.created ? (
-                <p>Created: {actionData.created.name}</p>
-            ) : null}
+            {
+                actionData?.error ? (
+                    <p style={{ color: "red" }}>{actionData.error}</p>
+                ) : actionData?.created ? (
+                    <p>Created: {actionData.created.name}</p>
+                ) : null
+            }
 
             <ul>
                 {users.map(u => (<li key={u.id}>#{u.id} {u.name} ({u.email})</li>))}
