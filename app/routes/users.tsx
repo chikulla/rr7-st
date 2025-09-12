1f796db2e49eb6268a2d02f00e088a0292624af6
chikulla <3042031+chikulla@users.noreply.github.com>
2025-09-12 23:44:46 +0900

    basic table struture

--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,21 +1,26 @@
+import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable, type ColumnDef } from "@tanstack/react-table";
 import type { Route } from "./+types/users";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
 import { useForm } from "react-hook-form";
-import { useSubmit } from "react-router";
-import { useEffect } from "react";
+import { useNavigation, useSubmit } from "react-router";
+import { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
 
-let mem = [
+type User = { id: number, name: string, email: string };
+
+let mem: User[] = [
     { id: 1, name: "Taro Tanaka", email: "taro@example.com" },
     { id: 2, name: "Hanako Kikuchi", email: "hanako@example.com" },
 ];
-export async function loader(): Promise<{ users: typeof mem }> {
+export async function loader(): Promise<{ users: User[] }> {
     return { users: mem }
 }
 
-export type ActionData = { error: string } | { created: typeof mem[number] };
+export type ActionData = { error: string } | { created: User };
 export async function action({ request }: Route.ActionArgs): Promise<ActionData> {
+    await new Promise(resolve => setTimeout(resolve, 500));
     const form = await request.formData(); // form data取り出し
     const name = String(form.get("name"));
     const email = String(form.get("email"));
@@ -26,8 +31,60 @@ export async function action({ request }: Route.ActionArgs): Promise<ActionData>
     mem = [...mem, user];
     return { created: user }
 }
+
+const cols: ColumnDef<User>[] = [
+    { accessorKey: "id", header: "ID" },
+    { accessorKey: "name", header: "Name" },
+    { accessorKey: "email", header: "Email" },
+]
+
+function DataTable<TData, TValue>({ columns, data }: { columns: ColumnDef<TData, TValue>[], data: TData[] }) {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+        <div>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                </TableHead>
+                            ))}
+                        </TableRow>))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length}>
+                                No results
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
+
 export default function Users({ loaderData, actionData }: Route.ComponentProps) {
     const { users } = loaderData;
+    const navigation = useNavigation();
     const form = useForm<{ name: string, email: string }>({ defaultValues: { name: "", email: "" } });
     const submit = useSubmit();
     function onSubmit(data: { name: string, email: string }) {
@@ -65,7 +122,7 @@ export default function Users({ loaderData, actionData }: Route.ComponentProps) 
                             <FormMessage />
                         </FormItem>
                     )} />
-                    <Button type="submit" disabled={form.formState.isSubmitted}>Add</Button>
+                    <Button type="submit" disabled={navigation.state === "submitting"}>Add</Button>
                 </form>
             </Form>
 
@@ -77,9 +134,7 @@ export default function Users({ loaderData, actionData }: Route.ComponentProps) 
                 ) : null
             }
 
-            <ul>
-                {users.map(u => (<li key={u.id}>#{u.id} {u.name} ({u.email})</li>))}
-            </ul>
+            <DataTable columns={cols} data={users} />
         </div >
     );
 }
