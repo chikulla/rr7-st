a80e57569602c3320a1b8db8a57c2e69f5d3706b
chikulla <3042031+chikulla@users.noreply.github.com>
2025-09-14 19:01:10 +0900

    redirect and pagination

--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,13 +1,19 @@
-import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable, type Column, type ColumnDef, type OnChangeFn, type SortingState } from "@tanstack/react-table";
+import {
+    flexRender, getCoreRowModel, getFilteredRowModel,
+    getPaginationRowModel, getSortedRowModel,
+    useReactTable, type Column, type ColumnDef,
+    type OnChangeFn, type SortingState, type PaginationState,
+} from "@tanstack/react-table";
 import type { Route } from "./+types/users";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
 import { useForm } from "react-hook-form";
-import { useNavigate, useNavigation, useSubmit } from "react-router";
+import { redirect, useLocation, useNavigate, useNavigation, useSubmit } from "react-router";
 import { useEffect, useState } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
 import { ArrowDown, ArrowUp, ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import { Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious } from "~/components/ui/pagination";
 
 
 type User = { id: number, name: string, email: string };
@@ -16,16 +22,18 @@ let mem: User[] = [
     { id: 1, name: "Taro Tanaka", email: "taro@example.com" },
     { id: 2, name: "Hanako Kikuchi", email: "hanako@example.com" },
 ];
-export async function loader({ request }: Route.LoaderArgs): Promise<{ users: User[]; sort?: string }> {
+export async function loader({ request }: Route.LoaderArgs): Promise<{ users: User[]; sort?: string, pages: number } | Response> {
     const url = new URL(request.url)
-    console.log(url.search)
+    if (!url.searchParams.get("_page") || !url.searchParams.get("_per_page")) {
+        return redirect(`${url.pathname}?_page=1&_per_page=10${url.search ? '&' + url.searchParams.toString() : ''}`);
+    }
     const sort = url.searchParams.get("_sort") ?? undefined;
     const r = await fetch(`http://localhost:6173${url.pathname}${url.search}`)
     if (r.ok) {
         const result = JSON.parse(await r.text());
-        return { users: result, sort }
+        return { users: result.data, pages: result.pages, sort }
     } else {
-        return { users: [] } // TODO: handle error
+        return { users: [], pages: 0 } // TODO: handle error
     }
 
 }
@@ -65,8 +73,15 @@ const cols: ColumnDef<User>[] = [
     { accessorKey: "email", header: sortableHeader("Email"), enableSorting: true },
 ]
 
-function DataTable<TData, TValue>({ columns, data }: { columns: ColumnDef<TData, TValue>[], data: TData[] }) {
+function DataTable<TData, TValue>({ columns, data, pages }: { columns: ColumnDef<TData, TValue>[], data: TData[], pages: number }) {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search)
+    const _sort = params.get("_sort")
+    const _page = params.get("_page") || "1"
+    const _perPage = params.get("_per_page") || "10"
+
     const [sorting, setSorting] = useState<SortingState>([]);
+    const [pagination, setPagination] = useState<PaginationState>({ pageIndex: _page ? parseInt(_page) - 1 : 0, pageSize: _perPage ? parseInt(_perPage) : 10 });
     const navigate = useNavigate();
     useEffect(() => {
         if (sorting.length === 0) {
@@ -75,52 +90,74 @@ function DataTable<TData, TValue>({ columns, data }: { columns: ColumnDef<TData,
         const s = sorting[0];
         navigate(`?_sort=${s.desc ? '-' : ''}${s.id}&_order=${s.desc ? "desc" : "asc"}`, { replace: true })
     }, [sorting])
+    useEffect(() => {
+        console.log("pagination changed: ", pagination)
+        navigate(`?_page=${pagination.pageIndex + 1}&_per_page=${pagination.pageSize}`, { replace: true })
+        // console.log("pagination changed: ", pagination)
+    }, [pagination])
     const table = useReactTable({
         data,
         columns,
+        pageCount: pages,
         getCoreRowModel: getCoreRowModel(),
         onSortingChange: setSorting,
-        state: { sorting }
+        onPaginationChange: setPagination,
+        manualPagination: true,
+        state: { sorting, pagination }
     })
 
 
     return (
-        <Table>
-            <TableHeader>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <TableHead key={header.id}>
-                                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                            </TableHead>
-                        ))}
-                    </TableRow>))}
-            </TableHeader>
-            <TableBody>
-                {table.getRowModel().rows?.length ? (
-                    table.getRowModel().rows.map(row => (
-                        <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                            {row.getVisibleCells().map(cell => (
-                                <TableCell key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </TableCell>
+        <div>
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                </TableHead>
                             ))}
+                        </TableRow>))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map(row => (
+                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                {row.getVisibleCells().map(cell => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results
+                            </TableCell>
                         </TableRow>
-                    ))
-                ) : (
-                    <TableRow>
-                        <TableCell colSpan={columns.length} className="h-24 text-center">
-                            No results
-                        </TableCell>
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+                    )}
+                </TableBody>
+            </Table>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious className={table.getCanPreviousPage() ? '' : 'pointer-events-none opacity-50'} onClick={() => table.previousPage()} />
+                    </PaginationItem>
+                    <PaginationItem>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext className={table.getCanNextPage() ? '' : 'pointer-events-none opacity-50'} onClick={() => table.nextPage()} />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </div>
     )
 }
 
 export default function Users({ loaderData, actionData }: Route.ComponentProps) {
-    const { users } = loaderData;
+    const { users, pages } = loaderData;
     const navigation = useNavigation();
     const form = useForm<{ name: string, email: string }>({ defaultValues: { name: "", email: "" } });
     const submit = useSubmit();
@@ -171,7 +208,7 @@ export default function Users({ loaderData, actionData }: Route.ComponentProps) 
                 ) : null
             }
 
-            <DataTable columns={cols} data={users} />
+            <DataTable columns={cols} data={users} pages={pages} />
         </div >
     );
 }
